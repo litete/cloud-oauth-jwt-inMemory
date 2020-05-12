d08302b46946b48dab202fd3e9eae739c149fbd3
litete <1153681927@qq.com>
2020-05-12 11:37:15 +0800

    first commit

---
+++ auth-server/src/main/java/com/example/authserver/config/OAuth2Config.java
@@ -0,0 +1,104 @@
+package com.example.authserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    public PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserDetailsService kiteUserDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtAccessTokenConverter jwtAccessTokenConverter;
+
+    @Autowired
+    private TokenStore jwtTokenStore;
+//    private TokenStore redisTokenStore;
+
+
+   /* @Autowired
+    private TokenEnhancer jwtTokenEnhancer;
+
+    @Bean
+    public TokenEnhancer jwtTokenEnhancer() {
+        return new JWTokenEnhancer();
+    }*/
+
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        /**
+//         * jwt 增强模式
+//        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+//        List<TokenEnhancer> enhancerList = new ArrayList<>();
+//        enhancerList.add(jwtTokenEnhancer);
+//        enhancerList.add(jwtAccessTokenConverter);
+//        enhancerChain.setTokenEnhancers(enhancerList);
+//        endpoints.tokenStore(jwtTokenStore)
+//                .userDetailsService(kiteUserDetailsService)
+//                *//**
+//                 * 支持 password 模式
+//                 *//*
+//                .authenticationManager(authenticationManager)
+//                .tokenEnhancer(enhancerChain)
+//                .accessTokenConverter(jwtAccessTokenConverter);*/
+
+        /**
+         * 普通 jwt 模式
+         */
+        endpoints.tokenStore(jwtTokenStore)
+                .accessTokenConverter(jwtAccessTokenConverter)
+                .userDetailsService(kiteUserDetailsService)
+                /**
+                 * 支持 password 模式
+                 */
+                .authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("order-client")
+                .secret(passwordEncoder.encode("order-secret-8888"))
+                .authorizedGrantTypes("refresh_token", "authorization_code", "password")
+                .accessTokenValiditySeconds(3600)
+                .scopes("all")
+                .and()
+                .withClient("user-client")
+                .secret(passwordEncoder.encode("user-secret-8888"))
+                .authorizedGrantTypes("refresh_token", "authorization_code", "password")
+                .accessTokenValiditySeconds(3600)
+                .scopes("all");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.allowFormAuthenticationForClients();
+        security.checkTokenAccess("isAuthenticated()");
+        security.tokenKeyAccess("isAuthenticated()");
+    }
+
+}
---
+++ user-server/src/main/java/com/example/userserver/config/ResourceServerConfig.java
@@ -0,0 +1,43 @@
+package com.example.userserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Bean
+    public TokenStore jwtTokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+
+        accessTokenConverter.setSigningKey("dev");
+        accessTokenConverter.setVerifierKey("dev");
+        return accessTokenConverter;
+    }
+
+    @Autowired
+    private TokenStore jwtTokenStore;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(jwtTokenStore);
+    }
+}
